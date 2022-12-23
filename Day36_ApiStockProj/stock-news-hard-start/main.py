95c0c82c0b74c011bdfd3e6c330d0ec437915101
jarngalrahul <rahuljarngal28@gmail.com>
2022-12-23 15:53:50 +0530

    init

--- Day36_ApiStockProj/stock-news-hard-start/main.py
+++ Day36_ApiStockProj/stock-news-hard-start/main.py
@@ -1,37 +1,42 @@
+import os
+import requests
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-
+NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
+STOCK_API_KEY = os.environ.get("STOCK_API_KEY")
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 
-## STEP 1: Use https://newsapi.org/docs/endpoints/everything
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-#HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
-#HINT 2: Work out the value of 5% of yerstday's closing stock price. 
+def main():
+    stock_params = {
+        "function": "TIME_SERIES_DAILY_ADJUSTED",
+        "symbol": STOCK,
+        "apikey": STOCK_API_KEY
+    }
+    r = requests.get(url="https://www.alphavantage.co/query",
+                     params=stock_params)
+    stock_data = r.json()['Time Series (Daily)']
+    data_list = [value for (key, value) in stock_data.items()]
+    yesterday_data = data_list[0]
+    cp_yest = float(yesterday_data['4. close'])
+    day_before_yesterday_data = data_list[1]
+    cp_day_before_yest = float(day_before_yesterday_data['4. close'])
+    gains = round(abs((1-cp_day_before_yest/cp_yest)*100))
+    if gains > 1:
+        news_params = {
+            "q": STOCK,
+            "apikey": NEWS_API_KEY,
+        }
+        results = requests.get(url=NEWS_ENDPOINT, params=news_params)
+        results.raise_for_status()
+        data = results.json()['articles'][:3]
+        formatted_msg = [
+            f"Title: {entry['title']}\nDescription: {entry['description']}" for entry in data]
+        for msg in formatted_msg:
+            print("\n\n", msg)
+            # we could use twilio to send msg if the service is available in your country
 
 
-
-## STEP 2: Use https://newsapi.org/docs/endpoints/everything
-# Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
-#HINT 1: Think about using the Python Slice Operator
-
-
-
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# Send a separate message with each article's title and description to your phone number. 
-#HINT 1: Consider using a List Comprehension.
-
-
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
+if __name__ == "__main__":
+    main()
